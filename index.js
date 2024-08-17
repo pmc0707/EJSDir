c1b6e3f0a30c90f53196e3c5ed0c4fed4e887191
pmc0707 <pranshugoodboy@gmail.com>
2024-08-17 12:39:31 +0530

    resolving of error by making views file

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 
 const port = 8080;
 
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
 app.get("/", (req, res) => {
-    res.render("home.html");
+    res.render("home.ejs");
 })
 app.listen(port, () => {
     console.log("Server running on port " + port);
