eeec70953f89397e140ddd178788d7c8a5f80df0
pmc0707 <pranshugoodboy@gmail.com>
2024-08-17 16:30:18 +0530

    passing data to ejs

--- index.js
+++ index.js
@@ -4,15 +4,17 @@ const path = require("path");
 
 const port = 8080;
 
+app.use(express.static("public"));
+
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 app.get("/", (req, res) => {
     res.render("home.ejs");
 })
-app.get("/rolldice", (req, res) => {
-    let diceVal=Math.floor(Math.random() * 4)+1;
-    res.render("rolldice.ejs", {num :diceVal});
+app.get("/", (req, res) => {
+    res.render("home.ejs");
 })
+
 app.listen(port, () => {
     console.log("Server running on port " + port);
 })
