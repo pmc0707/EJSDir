2808565d6dda2eedaef9c942a059d8bc352a87e9
pmc0707 <pranshugoodboy@gmail.com>
2024-08-17 15:23:58 +0530

    passing data to ejs

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ app.set("views", path.join(__dirname, "/views"));
 app.get("/", (req, res) => {
     res.render("home.ejs");
 })
+app.get("/rolldice", (req, res) => {
+    let diceVal=Math.floor(Math.random() * 4)+1;
+    res.render("rolldice.ejs", {num :diceVal});
+})
 app.listen(port, () => {
     console.log("Server running on port " + port);
 })
